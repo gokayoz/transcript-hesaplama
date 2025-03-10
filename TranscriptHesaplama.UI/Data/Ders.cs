a4c872e2660ebe426703a13397865ab63cf3709a
gokayoz <gokay.oz@outlook.com.tr>
2025-03-10 17:22:33 +0300

    Adds the necessary codes, form components and user interface for transcript calculation.

---
+++ TranscriptHesaplama.UI/Data/Ders.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TranscriptHesaplama.UI.Enums;
+
+namespace TranscriptHesaplama.UI.Data
+{
+    public class Ders
+    {
+        public string Ad { get; set; }
+
+        public string Kod { get; set; }
+
+        public uint Kredi { get; set; }
+
+        public uint Vize { get; set; }
+
+        public uint Final { get; set; }
+
+        public HarfNotu? HarfNotu { get; set; }
+    }
+}
---
+++ TranscriptHesaplama.UI/Enums/HarfNotu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TranscriptHesaplama.UI.Enums
+{
+    public enum HarfNotu
+    {
+        AA = 40,
+        BA = 35,
+        BB = 30,
+        CB = 25,
+        CC = 20,
+        DC = 15,
+        DD = 10,
+        FF = 0,
+    }
+}
